774f0bb175d0fcfb71c94b98e11ca83294468f25
Lars Hollingsed <larshollingsed@gmail.com>
2015-07-18 18:44:57 -0500

    Delete photographer view

--- controllers/photographer_controller.rb
+++ controllers/photographer_controller.rb
@@ -6,3 +6,13 @@ get "/add_photographer_confirm" do
   Photographer.create({"name" => params["photographer"]["name"]})
   erb :"/menu"
 end
+
+get "/delete_photographer" do
+  @photographers = Photographer.all
+  erb :"/photographers/delete_photographer"
+end
+
+get "/delete_photographer_confirm" do
+  Photographer.destroy(params["photographer"])
+  erb :"/menu"
+end
