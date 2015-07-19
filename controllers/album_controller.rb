f6d4cac85381eb70c52d3e095f54dba2381e32db
Lars Hollingsed <larshollingsed@gmail.com>
2015-07-18 20:28:34 -0500

    Delete album view

--- controllers/album_controller.rb
+++ controllers/album_controller.rb
@@ -6,3 +6,13 @@ get "/add_album_confirm" do
   Album.create({"name" => params["album"]["name"]})
   erb :"/menu"
 end
+
+get "/delete_album" do
+  @albums = Album.all
+  erb :"/albums/delete_album"
+end
+
+get "/delete_album_confirm" do
+  Album.destroy(params["album"]["name"])
+  erb :"/menu"
+end
