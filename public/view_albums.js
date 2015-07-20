1e3568de74c69020bf3e241e944fbaff01795ea6
Lars Hollingsed <larshollingsed@gmail.com>
2015-07-20 11:14:16 -0500

    Added documentation for view_albums.js (including top star)

--- public/view_albums.js
+++ public/view_albums.js
@@ -10,25 +10,42 @@ function loadAlbumInfo() {
   request.send(new FormData(formElement));
   
   request.addEventListener("load", function() {
+    // an Array of all elements with the "picture" class
     var all_photos = $(".picture");
+    
+    // an Array of all elements with the "top_photo_star" class
     var all_stars = $(".top_photo_star");
+    
+    // adds the "secret" to all photos
     for (var x = 0; x < all_photos.length; x++) {
       all_photos[x].classList.add("secret");
     }
     
+    // add the "secret" class to all top stars
     for (var x = 0; x < all_stars.length; x++) {
       all_stars[x].classList.add("secret");
     }
     
+    // gets the photo ids of all photos from this response (in the album selected)
     var photo_ids = this.response.photos;
-    for (var x = 0; x < photo_ids.length; x ++) {
+    
+    // loops through all photos in this album
+    for (var x = 0; x < photo_ids.length; x++) {
+      // creates a string for this specific photo's id
       this_photo = "#photo" + photo_ids[x];
+      
+      // removes the "secret" class from this specific photo (by id)
       $(this_photo).removeClass("secret")
+      
+      // creates a string for this specific photo's star (whether it exists or not)
       top_star = this_photo + "star"
+      
+      // if the star exists this removes the "secret" class from it
       if ($(top_star).length !== 0)  {
         $(top_star).removeClass("secret")
       }
     }
+    // displays the album name that the user chose
     $("#album_name")[0].innerText = this.response.name;
   });
 };
