6b581191c56d1f509b0fa67d26f573f63a51a395
Lars Hollingsed <larshollingsed@gmail.com>
2015-07-20 21:08:16 -0500

    Fixed bug for see all photos top_photo view

--- public/view_albums.js
+++ public/view_albums.js
@@ -7,21 +7,13 @@ function loadAlbumInfo() {
   
   request.addEventListener("load", function() {
     // an Array of all elements with the "picture" class
-    var all_photos = $("#photos_in_album div.photo_container");
-    
-    // an Array of all elements with the "top_photo_star" class
-    var all_stars = $(".top_photo_star");
+    var all_photos = $(".photo_container");
     
     // adds the "secret" class to all photos
     for (var x = 0; x < all_photos.length; x++) {
       all_photos[x].classList.add("secret");
       all_photos[x].classList.remove("not_secret")
     }
-    
-    // // add the "secret" class to all top stars
-    // for (var x = 0; x < all_stars.length; x++) {
-    //   all_stars[x].classList.add("secret");
-    // }
     
     // gets the photo ids of all photos from this response (in the album selected)
     var photo_ids = this.response.photos;
@@ -35,13 +27,6 @@ function loadAlbumInfo() {
       $(this_photo).removeClass("secret")
       $(this_photo).addClass("not_secret")
       
-      // creates a string for this specific photo's star (whether it exists or not)
-      // top_star = this_photo + "star"
-      //
-      // // if the star exists this removes the "secret" class from it
-      // if ($(top_star).length !== 0)  {
-      //   $(top_star).removeClass("secret")
-      // }
       // displays the album name that the user chose
       $("#album_name")[0].innerText = this.response.name;
       
